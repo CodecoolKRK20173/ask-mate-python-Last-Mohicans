f1253a271ad33b139b735c25c6d9d616c3e49c50
Papcio Chmiel <pfaderewski@gmail.com>
2019-03-12 13:22:28 +0100

    some more skeleton functions

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, redirect, request
+import data_manager
+import util
+
+app = Flask(__name__)
+
+
+@app.route('/list')
+def route_list():
+    pass
+
+
+@app.route('/question')
+@app.route('/question/<question_id>')
+@app.route('/question/<question_id>/edit')
+@app.route('/question/<question_id>/delete')
+@app.route('/question/<question_id>/new-answer')
+@app.route('/question/<question_id>/vote-up')
+@app.route('/question/<question_id>/vote-down')
+def route_question():
+    pass
+
+
+@app.route('/add-question')
+def route_list():
+    pass
+
+
+if __name__ == "__main__":
+    app.run(
+        host='0.0.0.0',
+        debug=True,  # Allow verbose error reports
+        port=8000  # Set custom port
+    )
