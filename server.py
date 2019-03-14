fddc664aea885e8f470b39862f845b0e461aa7a4
Papcio Chmiel <pfaderewski@gmail.com>
2019-03-14 13:05:06 +0100

    foto added

--- server.py
+++ server.py
@@ -8,6 +8,12 @@ import os
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = connection.UPLOAD_FOLDER
+app.secret_key = 'camel toe'
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in connection.ALLOWED_EXTENSIONS
 
 
 @app.route('/')
@@ -61,17 +67,10 @@ def answer_vote_down(answer_id):
 
 
 @app.route('/add-question', methods=['GET', 'POST'])
+# @app.route('/add-question/<filename>', methods=['GET', 'POST'])
 def route_add_question():
-    if request.method == 'POST':
 
-        values = [data_manager.get_new_question_id(),
-                  util.get_timestamp(),
-                  '0',
-                  '0',
-                  request.form['title'],
-                  request.form['message'],
-                  '']
-<<<<<<< HEAD
+    if request.method == 'POST':
 
         if 'file' not in request.files:
             flash('No file part')
@@ -80,21 +79,23 @@ def route_add_question():
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
-            # flash('No selected file')
+            flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            url = url_for('static', filename=filename)
 
-            # return render_template('list.html', questions=ordered_questions, filename=filename)
+        values = [data_manager.get_new_question_id(),
+                  util.get_timestamp(),
+                  '0',
+                  '0',
+                  request.form['title'],
+                  request.form['message'],
+                  filename]
 
-            values[6] = url
-
-=======
->>>>>>> 9006891589a7fff6d8a958a53ac2b50543ccb85c
         data_manager.add_question(values)
         return redirect('/')
+
     else:
         return render_template('add-question.html', id=None, question=None)
 
@@ -105,13 +106,28 @@ def route_edit_question(question_id):
         question = data_manager.get_question_by_id(question_id)
         question['title'] = request.form['title']
         question['message'] = request.form['message']
+
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            question['image'] = filename
         data_manager.update_question(question, question_id)
         return redirect('/')
     else:
-<<<<<<< HEAD
-        question_headers = data_manager.get_question_fields()[4:7]
-        return render_template('add-question.html', question_headers=question_headers)
-=======
+
+        # question_headers = data_manager.get_question_fields()[4:7]
+        # return render_template('add-question.html', question_headers=question_headers)
+
         question = data_manager.get_question_by_id(question_id)
         return render_template('add-question.html', id=question_id, question=question)
 
@@ -120,7 +136,6 @@ def route_edit_question(question_id):
 def route_remove_question(question_id):
     data_manager.remove_question(question_id)
     return redirect('/list')
->>>>>>> 9006891589a7fff6d8a958a53ac2b50543ccb85c
 
 
 @app.route('/question/<question_id>/new-answer', methods=['GET', 'POST'])
@@ -156,34 +171,30 @@ def route_remove_answer(answer_id):
 #
 #     return redirect('/list')
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in connection.ALLOWED_EXTENSIONS
 
-
-@app.route('/list', methods=['GET', 'POST'])
-def upload_file():
-    regular_questions = data_manager.get_questions()
-    regular_questions = {int(key): value for key, value in regular_questions.items()}
-    ordered_questions = util.reversed_order_dict(regular_questions)
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            # flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            url = url_for('static', filename=filename)
-
-            return render_template('list.html', questions=ordered_questions, filename=filename)
-    return render_template('list.html', questions=ordered_questions) #, filename=filename)
+# @app.route('/list', methods=['GET', 'POST'])
+# def upload_file():
+#     regular_questions = data_manager.get_questions()
+#     regular_questions = {int(key): value for key, value in regular_questions.items()}
+#     ordered_questions = util.reversed_order_dict(regular_questions)
+#     if request.method == 'POST':
+#         # check if the post request has the file part
+#         if 'file' not in request.files:
+#             flash('No file part')
+#             return redirect(request.url)
+#         file = request.files['file']
+#         # if user does not select file, browser also
+#         # submit a empty part without filename
+#         if file.filename == '':
+#             # flash('No selected file')
+#             return redirect(request.url)
+#         if file and allowed_file(file.filename):
+#             filename = secure_filename(file.filename)
+#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#             url = url_for('static', filename=filename)
+#
+#             return render_template('list.html', questions=ordered_questions, filename=filename)
+#     return render_template('list.html', questions=ordered_questions) #, filename=filename)
 
 
 if __name__ == "__main__":
